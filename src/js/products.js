16f028ecb89ef61268eb96d9dc80b2fc23ffe0b3
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-10-17 18:14:57 -0300

    sync - syncing with laptop

--- src/js/products.js
+++ src/js/products.js
@@ -3,6 +3,8 @@ const closeFilterModalButton = document.querySelector(
   "#closeFilterModalButton",
 );
 const filterModal = document.querySelector("#filterModal");
+const filterModalForm = document.querySelector("#filterModalForm");
+const filterModalRangeInputs = document.querySelectorAll(".form-range-input");
 
 function closeModal(modalElement) {
   modalElement.classList.add("hidden");
@@ -11,6 +13,49 @@ function closeModal(modalElement) {
 function openModal(modalElement) {
   modalElement.classList.remove("hidden");
 }
+
+function handleFilterModalFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const formDataObject = Object.fromEntries(formData);
+  console.log("FILTERING PRODUCTS WITH ", formDataObject);
+  closeModal(filterModal);
+}
+
+function loadFilterCategories() {
+  console.log("LOAD CATEGORIES CALLED");
+  const categoryDropdownFilter = document.querySelectorAll(
+    "#categoryDropdownFilter",
+  );
+  const categorySelect = categoryDropdownFilter[1];
+  const categoriesList = [
+    {
+      label: "Todas",
+      value: "all",
+    },
+    {
+      label: "Jardinagem",
+      value: "gardening",
+    },
+    {
+      label: "Limpeza",
+      value: "cleaning",
+    },
+    {
+      label: "Construção",
+      value: "construction",
+    },
+  ];
+  const categoriesListHTML = categoriesList
+    .map((category) => {
+      return `<option value="${category.value}">${category.label}</option>`;
+    })
+    .join("");
+
+  categorySelect.innerHTML = categoriesListHTML;
+}
+
+filterModalForm.addEventListener("submit", handleFilterModalFormSubmit);
 
 filterModalButton.addEventListener("click", () => {
   openModal(filterModal);
@@ -26,8 +71,21 @@ filterModal.addEventListener("click", (event) => {
   closeModal(filterModal);
 });
 
+filterModalRangeInputs.forEach((rangeInput) => {
+  rangeInput.addEventListener("input", (event) => {
+    const rangeInputValue = event.target.value;
+    const rangeInputId = event.target.id;
+    console.log(rangeInputValue);
+    console.log(rangeInputId);
+  });
+});
+
 window.addEventListener("keydown", (event) => {
   const filterModalClasses = filterModal.classList;
   if (event.key !== "Escape" || filterModalClasses.contains("hidden")) return;
   closeModal(filterModal);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadFilterCategories();
 });
