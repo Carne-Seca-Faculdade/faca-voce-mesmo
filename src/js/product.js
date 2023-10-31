5946d8f172575dad0c0464b65e90773ab04efe11
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-10-30 21:53:19 -0300

    fix: importando db do firestore.ts que já havia sido configurado anteriormente

--- src/js/product.js
+++ src/js/product.js
@@ -1,35 +1,35 @@
 function toggleModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.classList.toggle("hidden");
+  const modal = document.getElementById(modalId);
+  modal.classList.toggle("hidden");
 
-    function closeOnEscape(event) {
-        if (event.key === "Escape") {
-            modal.classList.add("hidden");
-            window.removeEventListener("keydown", closeOnEscape);
-        }
+  function closeOnEscape(event) {
+    if (event.key === "Escape") {
+      modal.classList.add("hidden");
+      window.removeEventListener("keydown", closeOnEscape);
     }
+  }
 
-    if (!modal.classList.contains("hidden")) {
-        window.addEventListener("keydown", closeOnEscape);
-    }
+  if (!modal.classList.contains("hidden")) {
+    window.addEventListener("keydown", closeOnEscape);
+  }
 }
 
 const productContactButton = document.querySelector("#productContactButton");
 productContactButton.addEventListener("click", () => {
-    toggleModal("contactModal");
+  toggleModal("contactModal");
 });
 
 const closeContactModal = document.querySelector("#closeContactModal");
 closeContactModal.addEventListener("click", () => {
-    toggleModal("contactModal");
+  toggleModal("contactModal");
 });
 
 const editModal = document.querySelector("#edit-product");
 editModal.addEventListener("click", () => {
-    toggleModal("editModal");
+  toggleModal("editModal");
 });
 
 const closeEditModal = document.querySelector("#closeEditModal");
 closeEditModal.addEventListener("click", () => {
-    toggleModal("editModal");
+  toggleModal("editModal");
 });
